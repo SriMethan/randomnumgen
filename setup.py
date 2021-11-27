8e15d56d40dc3948823f4495f4fea8a61fffdf1c
SriMethan <77402282+SriMethan@users.noreply.github.com>
2021-11-27 12:04:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from distutils.core import setup
+import setuptools
+import subprocess
+import os
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+cf_remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+assert "." in cf_remote_version
+
+assert os.path.isfile("cf_remote/version.py")
+with open("cf_remote/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{cf_remote_version}\n")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
   name = 'randomnumgen',         
@@ -13,7 +25,7 @@ setup(
   author = 'SriMethan',                   
   url = 'https://github.com/SriMethan/randomnumgen',  
   download_url = 'https://github.com/SriMethan/randomnumgen/archive/refs/heads/main.zip',    
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  keywords = ['RANDOMNUMGEN', 'MEANINGFULL', 'KEYWORDS'],   
   classifiers=[
     'Intended Audience :: Developers',      
     'License :: OSI Approved :: MIT License',   
