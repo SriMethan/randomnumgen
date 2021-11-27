52b44f2905ca33da488993d0a08113162d2c9c59
SriMethan <77402282+SriMethan@users.noreply.github.com>
2021-11-27 09:34:30 +0530

    Fix

--- setup.py
+++ setup.py
@@ -1,28 +1,18 @@
-import os
-import io
-import setuptools
-
-def read_description():
-  description = io.open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8').read()
-  return description
-
-setuptools.setup(
-    name="randomnumgen",
-    version="0.1",
-    author="SriMethan",
-    description="A Python3 library that generates random numbers.",
-    long_description= read_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/SriMethan/randomnumgen",
-    project_urls={
-        "Bug Tracker": "hhttps://github.com/SriMethan/randomnumgen/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "randomnumgen"},
-    packages=setuptools.find_packages(where="randomnumgen"),
-    python_requires=">=3",
+from distutils.core import setup
+setup(
+  name = 'randomnumgen',         
+  packages = ['randomnumgen'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'A Python3 library that generates random numbers.',   
+  author = 'SriMethan',                   
+  url = 'https://github.com/SriMethan/randomnumgen',  
+  download_url = 'https://github.com/SriMethan/randomnumgen/archive/refs/heads/main.zip',    
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.7',
+  ],
 )
