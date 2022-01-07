8240c916fc586c52ce5639f71382f3799c160330
SriMethan <77402282+SriMethan@users.noreply.github.com>
2022-01-07 11:58:52 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="randomnumgen",
-    version="1.1.1",
+    version="1.1.3",
     author="SriMethan",
     description="A Python3 library that generates random numbers",
     long_description=long_description,
